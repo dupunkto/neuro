41f91462ae8ddc6acfc11c4876d1199036c5e02b
Robin <robindev2019@outlook.com>
2026-07-25 19:07:23 +0200

    Add helpers for working with colors

--- color.php
+++ color.php
@@ -1,6 +1,43 @@
 <?php
 // Utitities for working with HEX-colors.
 
+function normalize_color($value) {
+  if(!preg_match('/^#[0-9a-fA-F]{6}([0-9a-fA-F]{2})?$/', $value)) return null;
+  return strtolower(substr(trim($value), 0, 7));
+}
+
+function named_color($name) {
+  return match(strtolower(trim($name))) {
+    "black" => "#000000",
+    "silver" => "#c0c0c0",
+    "gray" => "#808080",
+    "white" => "#ffffff",
+    "maroon" => "#800000",
+    "red" => "#ff0000",
+    "purple" => "#800080",
+    "fuchsia" => "#ff00ff",
+    "green" => "#008000",
+    "lime" => "#00ff00",
+    "olive" => "#808000",
+    "yellow" => "#ffff00",
+    "navy" => "#000080",
+    "blue" => "#0000ff",
+    "teal" => "#008080",
+    "aqua", "cyan" => "#00ffff",
+    "orange" => "#ffa500",
+    "pink" => "#ffc0cb",
+    "magenta" => "#ff00ff",
+    "indigo" => "#4b0082",
+    "violet" => "#ee82ee",
+    "gold" => "#ffd700",
+    "coral" => "#ff7f50",
+    "tomato" => "#ff6347",
+    "salmon" => "#fa8072",
+    "khaki" => "#f0e68c",
+    default => null
+  };
+}
+
 function lighten($hex, $percent) {
   $hex = str_replace('#', '', $hex);
   $r = hexdec(substr($hex, 0, 2));
